1bfa27d959bceda3f78f595620845545b29a3c60
Jimmy McDermott <mcdappdev@gmail.com>
2020-02-16 08:16:32 -0500

    Remove swift 3 versions

--- Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "Leaf",
-    dependencies: [
-        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/node.git", majorVersion: 2),
-    ]
-)
